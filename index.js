d8f94d4a8eb669ae054af969f47176591f88c4df
Makarand Gijam <makarand.gijam@unitrestech.com>
2025-02-07 14:20:38 +0530

    added errors

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my Express server!");
+  res.status(200).json({ message: "Welcome to my Express server!" });
 });
 
 app.get("/message", (req, res) => {
-  res.json({ message: "Hello from Express!" });
+  res.status(400).json({ message: "Hello from Express!" });
+});
+
+app.get("/test", (req, res) => {
+  res.status(500).json({ message: "Error" });
 });
 
 // Start the server
